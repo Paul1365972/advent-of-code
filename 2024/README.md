2f5c4425d487e2da92a0fadf67ac0d6c547f3278
Paul1365972 <paul1365972@gmail.com>
2024-12-05 13:47:14 +0100

    Solve day 05 of aoc2024

--- 2024/README.md
+++ 2024/README.md
@@ -4,4 +4,18 @@ First Advent of Code I am participating in.
 
 ## Day 1/2/3
 
+Total: ~ 100 min
+
 Caught up the first three doors, the solutions are implemented in Rust with the goal of just getting the answer as quickly as possible.
+
+## Day 4
+
+Part 1/2: 30 min
+
+Finally wrote code for part 1 that is more easily adaptable for any part 2.
+
+## Day 5 ()
+
+Part 1: 25 min, Part 2: 3 min
+
+The solution is awful time complexity wise O(n^2 + m), but was really easy to quickly implement!
---
+++ 2024/day-05/src/part2.rs
@@ -0,0 +1,47 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+fn main() {
+    let str = include_str!("../input.txt");
+
+    let mut lines = str.lines();
+    let rules: Vec<(u8, u8)> = lines
+        .by_ref()
+        .take_while(|line| !line.is_empty())
+        .map(|line| {
+            line.split("|")
+                .map(|x| x.parse::<u8>().unwrap())
+                .next_tuple()
+                .unwrap()
+        })
+        .collect_vec();
+
+    let updates = lines
+        .map(|line| {
+            line.split(",")
+                .map(|x| x.parse::<u8>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let rules: HashSet<(u8, u8)> = rules.into_iter().collect();
+
+    let mut sum = 0;
+    for mut update in updates {
+        let mut fixed = false;
+        for i in 0..update.len() {
+            for j in i..update.len() {
+                if rules.contains(&(update[j], update[i])) {
+                    update.swap(i, j);
+                    fixed = true;
+                }
+            }
+        }
+        if fixed {
+        sum += update[(update.len() - 1) / 2] as usize;
+        }
+    }
+
+    println!("Result: {sum}");
+}
