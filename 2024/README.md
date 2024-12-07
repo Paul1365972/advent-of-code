9cae3f7f5b94fb690e610a67a31f712dda0a2df2
Paul1365972 <paul1365972@gmail.com>
2024-12-07 15:42:56 +0100

    Solve day 07 of aoc2024

--- 2024/README.md
+++ 2024/README.md
@@ -14,8 +14,16 @@ Part 1/2: 30 min
 
 Finally wrote code for part 1 that is more easily adaptable for any part 2.
 
-## Day 5 ()
+## Day 5
 
 Part 1: 25 min, Part 2: 3 min
 
 The solution is awful time complexity wise O(n^2 + m), but was really easy to quickly implement!
+
+## Day 6
+
+Part 1: 25 min, Part 2: 10 min
+
+## Day 7
+
+Part 1: 15 min, Part 2: 1 min
---
+++ 2024/day-07/src/part2.rs
@@ -0,0 +1,38 @@
+use itertools::Itertools;
+use rustc_hash::FxHashSet;
+
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let equations = input
+        .lines()
+        .map(|line| {
+            let mut split = line.split(": ");
+            (
+                split.next().unwrap().parse::<u64>().unwrap(),
+                split
+                    .next()
+                    .unwrap()
+                    .split_ascii_whitespace()
+                    .map(|num| num.parse::<u64>().unwrap())
+                    .collect_vec(),
+            )
+        })
+        .collect_vec();
+
+    let mut sum = 0;
+    for (result, mut nums) in equations {
+        let mut solutions = FxHashSet::from_iter([nums.remove(0)]);
+        for num in nums {
+            for solution in solutions.drain().collect_vec() {
+                solutions.insert(solution + num);
+                solutions.insert(solution * num);
+                solutions.insert(format!("{solution}{num}").parse().unwrap());
+            }
+        }
+        if solutions.contains(&result) {
+            sum += result;
+        }
+    }
+    println!("Result: {sum}");
+}
